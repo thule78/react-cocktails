a248ea14ed502074ed13b4ca8ee3b3b146d3cff7
thule <lunthu@gmail.com>
2019-05-29 12:23:26 +0700

    search bar

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+
+import SearchBar from './SearchBar';
+
+class App extends Component {
+  render(){
+    return(
+      <div>
+        <h1>Hello Cocktails</h1>
+        <SearchBar />
+      </div>
+
+      )
+  }
+}
+
+export default App
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component{
+
+  state = {
+    term: ""
+  }
+
+  handleChange = (event) => {
+    this.setState({term: event.target.value})
+  }
+
+  handleSubmit = (e)=>{
+    e.preventDefault();
+  }
+
+  render(){
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.term} onChange={this.handleChange}/>
+        </form>
+      </div>
+      )
+  }
+}
+
+export default SearchBar
